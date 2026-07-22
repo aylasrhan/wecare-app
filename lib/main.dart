444d808b7f0f4f9c702ba253acc591f7604c0608
Ayla Sarhan <alaa.srh123@gmail.com>
2026-07-22 15:05:13 -0700

    upcoming appointments

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart'; // لا تنسي عمل import
+import 'package:flutter_bloc/flutter_bloc.dart';     
 import 'package:wecare/core/services/auth_service.dart';
 import 'package:wecare/features/auth/login/presentation/cubit/auth_bloc.dart';
-import 'package:wecare/features/doctor/presentation/ui/view/doctor_appointments_screen.dart';
 import 'package:wecare/features/onboarding/presentation/ui/view/onboarding_screen.dart';
 
 void main() {
